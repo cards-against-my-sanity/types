f64fcd0d4ef316fbbe29ab2d8bc498679d37360f
Jacob Andersen <jacob@algorithmjunkie.com>
2023-09-13 20:31:52 +0800

    (dj khaled) another one

--- game/game.interface.ts
+++ game/game.interface.ts
@@ -2,12 +2,12 @@ import DecksPayload from "../deck/decks.payload";
 import GameSettingsPayload from "./game-settings.payload";
 import PlayersPayload from "./player/players.payload";
 import SpectatorsPayload from "./spectator/spectators.payload";
-import PartialPlayerPayload from "./player/partial-player.payload";
 import { GameState } from "./game-state.enum";
+import IPlayer from "./player/player.interface";
 
 export default interface IGame extends DecksPayload, GameSettingsPayload, PlayersPayload, SpectatorsPayload {
     id: string;
-    host: PartialPlayerPayload;
+    host: Partial<IPlayer>;
     state: GameState;
     roundNumber: number;
 }
