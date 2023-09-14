6aae7c8eb0e3350060d19ea2c44afd42cdf7b426
Jacob Andersen <jacob@algorithmjunkie.com>
2023-09-14 16:24:51 +0800

    cowabunga

--- game/component/message/message.interface.ts
+++ game/component/message/message.interface.ts
@@ -1,8 +1,9 @@
 import PartialPlayerPayload from "../../player/partial-player.payload";
+import IPlayer from "../../player/player.interface";
 import TimestampPayload from "../timestamp.payload";
 
 export default interface IMessage extends TimestampPayload {
     type: "chat" | "system",
     content: string;
-    sender?: PartialPlayerPayload;
+    sender?: Partial<IPlayer>;
 }
