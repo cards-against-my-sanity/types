0abff4bcb76871403c85ff49caee9d26679dad11
Jacob Andersen <jacob@algorithmjunkie.com>
2023-09-15 14:37:32 +0800

    add two new settings and rename two others

--- game/game-settings.interface.ts
+++ game/game-settings.interface.ts
@@ -2,7 +2,9 @@ export default interface IGameSettings {
     maxPlayers: number;
     maxSpectators: number;
     maxScore: number;   
-    roundIntermissionSeconds: number;
-    gameWinIntermissionSeconds: number;
+    roundIntermissionTimer: number;
+    gameWinIntermissionTimer: number;
+    playingTimer: number;
+    judgingTimer: number;
     allowPlayersToJoinMidGame: boolean;
 }
